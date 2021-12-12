a06980aa04686be7404f234b6f48a556c1bb5a40
mfelsch1 <mefe3055@colorado.edu>
2021-12-11 21:10:58 -0700

    Attempted following a tutorial, got confused, now attempting to follow pseudocode created with Julien

---
+++ Assets/Scripts/TorchesScripts/AttemptedScripts/RoomTemplates.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] tRooms;
+    public GameObject[] bRooms;
+    public GameObject[] lRooms;
+    public GameObject[] rRooms;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TorchesScripts/RoomManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    Dictionary<int, Vector2Int> directions = new Dictionary<int, Vector2Int>()
+    {
+        {
+            1, Vector2Int.up * 16
+        },
+        {
+            2, Vector2Int.down * 16
+        },
+        {
+            3, Vector2Int.right * 16
+        },
+        {
+            4, Vector2Int.left * 16
+        }
+    };
+
+    [SerializeField] private List<GameObject> topRooms;
+    [SerializeField] private List<GameObject> bottomRooms;
+    [SerializeField] private List<GameObject> rightRooms;
+    [SerializeField] private List<GameObject> leftRooms;
+    [SerializeField] private int maxRooms;
+    
+    private Vector2Int startingRoom = new Vector2Int(0, 0);
+
+    private List<Vector2Int> rooms = new List<Vector2Int>();
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // call drunkenWalk1, drunkenWalk2, then capRooms
+    }
+
+    // create half the rooms
+    void drunkenWalk1()
+    {
+        
+    }
+
+    // create the rest from the first half of rooms
+    void drunkenWalk2()
+    {
+        
+    }
+
+    // Cap any holes left from openings in rooms after creation
+    void capRooms()
+    {
+        
+    }
+    
+    // checking if there's an already existing room at that position
+    bool hasRoom(Vector2Int pos)
+    {
+        foreach (var room in rooms)
+        {
+            if (room == pos)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
