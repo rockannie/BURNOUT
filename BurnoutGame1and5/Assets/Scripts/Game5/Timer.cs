ba8cd5116668a151e08e58e414ac965afce32623
bvjhansisaketa <bvjhansisaketa@gmail.com>
2021-12-13 23:41:32 -0700

    change in project settings

---
+++ BurnoutGame1and5/Assets/Scripts/Game5/Timer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 2*60f;
+
+    public Text timertext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(timerGame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    IEnumerator timerGame()
+    {
+        for (float i = timer; i >= 0f; i -= 1)
+        {
+            string k = i.ToString();
+            timertext.text = "Timer: "+k;
+            if (i < 30)
+            {
+                timertext.color=Color.red;
+            }
+
+            if (i == 0)
+            {
+                if(SceneManager.GetActiveScene().name=="MiniGame1")
+                    SceneManager.LoadScene("gameoverforgame1");
+                if(SceneManager.GetActiveScene().name=="MiniGame5")
+                    SceneManager.LoadScene("gameoverforgame5");
+            }
+            yield return new WaitForSeconds(1f);
+        }
+        
+    }
+}
